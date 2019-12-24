0a17be0dccd32f15d32149ad9453aad3f2904172
Tadukoo <realtadukoo@gmail.com>
2019-12-24 15:19:04 -0500

    Added BooleanUtil and Javadoc'd AutoCloseableUtil
    - BooleanUtil holds methods for checking that a Boolean is/isn't
    true/false while properly handling nulls

--- TadukooUtil/src/com/gmail/realtadukoo/util/AutoCloseableUtil.java
+++ TadukooUtil/src/com/gmail/realtadukoo/util/AutoCloseableUtil.java
@@ -1,7 +1,18 @@
 package com.gmail.realtadukoo.util;
 
-public class AutoCloseableUtil {
+/**
+ * Util functions for dealing with AutoCloseables.
+ * 
+ * @author Logan Ferree (Tadukoo)
+ * @version 0.1-Alpha-SNAPSHOT
+ */
+public class AutoCloseableUtil{
 	
+	/**
+	 * Closes the given AutoCloseable while suppressing any error messages
+	 * 
+	 * @param autoCloseable The AutoCloseable to close
+	 */
 	public static final void closeQuietly(AutoCloseable autoCloseable){
 		if(autoCloseable != null){
 			try{
