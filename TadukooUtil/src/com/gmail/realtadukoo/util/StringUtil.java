bc45807bb3532c29a2e329086ee4570a8f08ea81
Tadukoo <realtadukoo@gmail.com>
2019-12-24 19:54:14 -0500

    Added more useful functions to StringUtil
    - Functions for dealing with converting to/from a comma-separated list
    of strings, and related functions

--- TadukooUtil/src/com/gmail/realtadukoo/util/StringUtil.java
+++ TadukooUtil/src/com/gmail/realtadukoo/util/StringUtil.java
@@ -1,12 +1,15 @@
 package com.gmail.realtadukoo.util;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 /**
  * Util functions for dealing with or building Strings.
  * 
  * @author Logan Ferree (Tadukoo)
- * @version Pre-Alpha
+ * @version 0.1-Alpha-SNAPSHOT
+ * @since Pre-Alpha
  */
 public class StringUtil{
 	
@@ -51,5 +54,59 @@ public class StringUtil{
 	 */
 	public static String buildStringWithNewLines(Collection<String> lines){
 		return buildStringWithSeparator(lines, "\n");
+	}
+	
+	/**
+	 * Builds a string from the given collection of Strings where each is separated by 
+	 * a comma.
+	 * Uses {@link #buildStringWithSeparator} to accomplish this.
+	 * <br><br>
+	 * e.g. buildCommaSeparatedString(Arrays.asList("foo", "bar", "baz") would produce:
+	 * <br>
+	 * "foo,bar,baz"
+	 * 
+	 * @param lines The lines to make into a single string
+	 * @return A String of the given lines as a comma-separated string
+	 */
+	public static String buildCommaSeparatedString(Collection<String> lines){
+		return buildStringWithSeparator(lines, ",");
+	}
+	
+	/**
+	 * Parses the given text into a List using the given separator to split and optionally 
+	 * trimming to remove whitespace in the resulting strings.
+	 * 
+	 * @param text The text to parse into a List of Strings
+	 * @param separator The separator String to use in separating the given text
+	 * @param trim Whether to trim any whitespace off the resulting strings
+	 * @return The resulting List of Strings
+	 */
+	public static List<String> parseListFromStringWithSeparator(String text, String separator, boolean trim){
+		List<String> strings = new ArrayList<>();
+		
+		// If there is no instance of the separator string, just make a list with the text
+		if(!text.contains(separator)){
+			// Optionally trim the text
+			strings.add(trim?text.trim():text);
+		}else{
+			// Split the text on the separator string
+			String[] textSplit = text.split(separator);
+			for(String split: textSplit){
+				// Add each string to the list, optionally trimming them
+				strings.add(trim?split.trim():split);
+			}
+		}
+		
+		return strings;
+	}
+	
+	/**
+	 * Parses a comma-separated list into a List of Strings.
+	 * 
+	 * @param text The text to convert into a List of Strings
+	 * @return The List of Strings produced by parsing the text
+	 */
+	public static List<String> parseCommaSeparatedListFromString(String text){
+		return parseListFromStringWithSeparator(text, ",", true);
 	}
 }
