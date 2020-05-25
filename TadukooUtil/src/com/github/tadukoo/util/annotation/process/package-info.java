58e50f6bb91d8915490ce904b72b067def3cdf7d
Tadukoo <realtadukoo@gmail.com>
2020-05-25 15:37:01 -0400

    Improved Parent Pom and Fixed a com.gmail.realtadukoo reference

--- TadukooUtil/src/com/github/tadukoo/util/annotation/process/package-info.java
+++ TadukooUtil/src/com/github/tadukoo/util/annotation/process/package-info.java
@@ -1,8 +1,8 @@
 /**
- * The Annotation processors for the Annotations in the {@link com.gmail.realtadukoo.util.annotation annotation package}. 
+ * The Annotation processors for the Annotations in the {@link com.github.tadukoo.util.annotation annotation package}.
  * Also includes 
- * {@link com.gmail.realtadukoo.util.annotation.process.AbstractAnnotationProcessor AbstractAnnotationProcessor} 
+ * {@link com.github.tadukoo.util.annotation.process.AbstractAnnotationProcessor AbstractAnnotationProcessor}
  * to use in making more annotation processors and simplify the related utils for it via including 
- * {@link com.gmail.realtadukoo.util.annotation.process.AnnotationUtil AnnotationUtil}.
+ * {@link com.github.tadukoo.util.annotation.process.AnnotationUtil AnnotationUtil}.
  */
 package com.github.tadukoo.util.annotation.process;
