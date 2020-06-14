7644d9aeaf261d25d1dffa06833083e5669a6748
Tadukoo <realtadukoo@gmail.com>
2020-06-14 19:48:59 -0400

    Added Some Tuples
    - Added Pair and Triple to TadukooLang for usage
    - Added maven central repository submission stuff to the parent pom

---
+++ TadukooLang/src/com/github/tadukoo/util/tuple/Pair.java
@@ -0,0 +1,113 @@
+package com.github.tadukoo.util.tuple;
+
+/**
+ * A tuple class for holding 2 items of specified type.
+ *
+ * @param <L> The left item type
+ * @param <R> The right item type
+ *
+ * @author Logan Ferree (Tadukoo)
+ * @version 0.1-Alpha-SNAPSHOT
+ */
+public class Pair<L, R>{
+	/** The left item in the tuple */
+	private L left;
+	/** The right item in the tuple */
+	private R right;
+	
+	/**
+	 * Constructs a Pair of the two given objects.
+	 *
+	 * @param left The left item in the pair
+	 * @param right The right item in the pair
+	 */
+	private Pair(L left, R right){
+		this.left = left;
+		this.right = right;
+	}
+	
+	/**
+	 * Constructs a Pair of the two given objects.
+	 *
+	 * @param left The left item in the Pair
+	 * @param right The right item in the Pair
+	 * @param <L> The type of the left item
+	 * @param <R> The type of the right item
+	 * @return A Pair containing the two given items
+	 */
+	public static <L, R> Pair<L, R> of(L left, R right){
+		return new Pair<>(left, right);
+	}
+	
+	/**
+	 * @return The left item from the Pair
+	 */
+	public L getLeft(){
+		return left;
+	}
+	
+	/**
+	 * @return The left item (key) from the Pair
+	 */
+	public L getKey(){
+		return left;
+	}
+	
+	/**
+	 * Sets the left item of the Pair
+	 *
+	 * @param left The left item to set
+	 */
+	public void setLeft(L left){
+		this.left = left;
+	}
+	
+	/**
+	 * Sets the left item (key) of the Pair
+	 *
+	 * @param key The left item (key) to set
+	 */
+	public void setKey(L key){
+		left = key;
+	}
+	
+	/**
+	 * @return The right item of the Pair
+	 */
+	public R getRight(){
+		return right;
+	}
+	
+	/**
+	 * @return The right item (value) of the Pair
+	 */
+	public R getValue(){
+		return right;
+	}
+	
+	/**
+	 * Sets the right item of the Pair
+	 *
+	 * @param right The right item to set
+	 */
+	public void setRight(R right){
+		this.right = right;
+	}
+	
+	/**
+	 * Sets the right item (value) of the Pair
+	 *
+	 * @param value The right item (value) to set
+	 */
+	public void setValue(R value){
+		right = value;
+	}
+	
+	/**
+	 * @return A string representation of the Pair as ({left item}, {right item})
+	 */
+	@Override
+	public String toString(){
+		return "(" + left.toString() + ", " + right.toString() + ")";
+	}
+}
---
+++ TadukooLang/src/com/github/tadukoo/util/tuple/Triple.java
@@ -0,0 +1,104 @@
+package com.github.tadukoo.util.tuple;
+
+/**
+ * A tuple class for holding 3 items of specified type.
+ *
+ * @param <L> The left item type
+ * @param <M> The middle item type
+ * @param <R> The right item type
+ *
+ * @author Logan Ferree (Tadukoo)
+ * @version 0.1-Alpha-SNAPSHOT
+ */
+public class Triple<L, M, R>{
+	/** The left item in the tuple */
+	private L left;
+	/** The middle item in the tuple */
+	private M middle;
+	/** The right item in the tuple */
+	private R right;
+	
+	/**
+	 * Constructs a Triple of the three given objects.
+	 *
+	 * @param left The left item in the triple
+	 * @param middle The middle item in the triple
+	 * @param right The right item in the triple
+	 */
+	private Triple(L left, M middle, R right){
+		this.left = left;
+		this.middle = middle;
+		this.right = right;
+	}
+	
+	/**
+	 * Constructs a Triple of the three given objects.
+	 *
+	 * @param left The left item in the Triple
+	 * @param middle The middle item in the Triple
+	 * @param right The right item in the Triple
+	 * @param <L> The type of the left item
+	 * @param <M> The type of the middle item
+	 * @param <R> The type of the right item
+	 * @return A Triple containing the three given items
+	 */
+	public static <L, M, R> Triple<L, M, R> of(L left, M middle, R right){
+		return new Triple<>(left, middle, right);
+	}
+	
+	/**
+	 * @return The left item from the Triple
+	 */
+	public L getLeft(){
+		return left;
+	}
+	
+	/**
+	 * Sets the left item of the Triple
+	 *
+	 * @param left The left item to set
+	 */
+	public void setLeft(L left){
+		this.left = left;
+	}
+	
+	/**
+	 * @return The middle item from the Triple
+	 */
+	public M getMiddle(){
+		return middle;
+	}
+	
+	/**
+	 * Sets the middle item of the Triple
+	 *
+	 * @param middle The middle item to set
+	 */
+	public void setMiddle(M middle){
+		this.middle = middle;
+	}
+	
+	/**
+	 * @return The right item from the Triple
+	 */
+	public R getRight(){
+		return right;
+	}
+	
+	/**
+	 * Sets the right item of the Triple
+	 *
+	 * @param right The right item to set
+	 */
+	public void setRight(R right){
+		this.right = right;
+	}
+	
+	/**
+	 * @return A string representation of the Triple as ({left item}, {middle item}, {right item})
+	 */
+	@Override
+	public String toString(){
+		return "(" + left.toString() + ", " + middle.toString() + ", " + right.toString() + ")";
+	}
+}
