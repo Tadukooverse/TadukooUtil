7ba567d67ab3e517c964a05c2dff9044498d7dd6
Tadukoo <realtadukoo@gmail.com>
2020-10-09 19:41:29 -0400

    Added package infos to Tadukoo View

---
+++ TadukooView/src/com/github/tadukoo/util/view/font/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Classes for dealing with Fonts. See {@link com.github.tadukoo.util.view.font.FontFamily FontFamily} and
+ * {@link com.github.tadukoo.util.view.font.FontResourceLoader FontResourceLoader} for more information, and
+ * see the {@link com.github.tadukoo.util.view.font.FontFamilies FontFamilies} enum for a collection of FontFamilies
+ * that can be used when using <a href="https://github.com/Tadukooverse/TadukooFonts" >Tadukoo Fonts</a>.
+ */
+package com.github.tadukoo.util.view.font;
