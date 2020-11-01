6759726ec335452237fea843deb3704ec8163a0b
Tadukoo <realtadukoo@gmail.com>
2020-11-01 13:04:13 -0500

    isBlank and isNotBlank in ListUtil
    - Issue #37

--- TadukooLang/src/com/github/tadukoo/util/ListUtil.java
+++ TadukooLang/src/com/github/tadukoo/util/ListUtil.java
@@ -8,12 +8,33 @@ import java.util.List;
  * Util functions for dealing with {@link List}s.
  * 
  * @author Logan Ferree (Tadukoo)
- * @version 0.1-Alpha-SNAPSHOT
+ * @version Alpha v.0.2
+ * @since Alpha v.0.1
  */
 public final class ListUtil{
 	
 	// Not allowed to create a ListUtil
 	private ListUtil(){ }
+	
+	/**
+	 * Checks if the given List is blank (either null or an empty list).
+	 *
+	 * @param list The List to check
+	 * @return true if the List is null or empty
+	 */
+	public static boolean isBlank(List<?> list){
+		return list == null || list.isEmpty();
+	}
+	
+	/**
+	 * Checks if the given List is NOT blank (blank = either null or an empty List).
+	 *
+	 * @param list The List to check
+	 * @return true if the List is not null and not empty
+	 */
+	public static boolean isNotBlank(List<?> list){
+		return !isBlank(list);
+	}
 	
 	/**
 	 * Creates a {@link List} using the given array of arguments. 
