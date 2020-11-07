a3e9781ad433f4afd8833267dd1141eae9133a8c
Tadukoo <realtadukoo@gmail.com>
2020-11-07 14:46:34 -0500

    TadukooTable
    - Added TadukooTable
    - Issue #47

---
+++ TadukooView/src/com/github/tadukoo/util/view/components/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * Contains components that provide extra customizations via Tadukoo Look & Feel (e.g.
+ * {@link com.github.tadukoo.util.view.shapes.ShapeInfo ShapeInfo}) and/or methods to make it easier to deal with
+ * the components (e.g. the add row methods in
+ * {@link com.github.tadukoo.util.view.components.TadukooTable TadukooTable})
+ */
+package com.github.tadukoo.util.view.components;
