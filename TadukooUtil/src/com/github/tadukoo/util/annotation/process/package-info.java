2218ce27e6a8d59ef5d4f53d0a8a48058c036dd5
Tadukoo <realtadukoo@gmail.com>
2020-12-13 16:41:05 -0500

    Removed TadukooAnnotationProcessor (and updated poms to Alpha v.0.3 SNAPSHOT)
    - Issue #70

--- TadukooUtil/src/com/github/tadukoo/util/annotation/process/package-info.java
+++
@@ -1,8 +0,0 @@
-/**
- * The Annotation processors for the Annotations in the {@link com.github.tadukoo.util.annotation annotation package}.
- * Also includes 
- * {@link com.github.tadukoo.util.annotation.process.AbstractAnnotationProcessor AbstractAnnotationProcessor}
- * to use in making more annotation processors and simplify the related utils for it via including 
- * {@link com.github.tadukoo.util.annotation.process.AnnotationUtil AnnotationUtil}.
- */
-package com.github.tadukoo.util.annotation.process;
