48a464fb6a7f79a0a1f246fe2a86aa3091b44bc1
Tadukoo <realtadukoo@gmail.com>
2020-12-14 20:58:00 -0500

    Cleanup of TadukooFunctions
    - Part of Issue #72

--- TadukooFunctions/junit/com/github/tadukoo/util/functional/predicate/ThrowingPredicateTest.java
+++ TadukooFunctions/junit/com/github/tadukoo/util/functional/predicate/ThrowingPredicateTest.java
@@ -3,7 +3,10 @@ package com.github.tadukoo.util.functional.predicate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 public class ThrowingPredicateTest{
 	private ThrowingPredicate<Integer, IllegalArgumentException> thrower;
